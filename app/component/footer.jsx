ba1fb5443dcba8875a6815bafa4e8b1ad749a9bc
pappu-DD <codeactionwala@gmail.com>
2025-03-20 23:45:05 +0530

    some footer added

--- app/component/footer.jsx
+++ app/component/footer.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      div
+    <div className='flex border-2 border-yellow-700 items-center  justify-between'>
+      <div className='p-4 border-1 border-red-600'>Logo</div>
+      <div className='p-4 border-1 border-red-600'>Support</div>
+      <div className='p-4 border-1 border-red-600'>useful links</div>
+      <div className='p-4 border-1 border-red-600'>Contact</div>
     </div>
   )
 }
